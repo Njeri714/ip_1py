6a3c8099b5d09b2dc2f85f5923a52cb6a265a858
njeri waweru <wawerunjeri714@gmail.com>
2022-01-28 09:11:08 +0300

    added content

--- creditials.py
+++ creditials.py
@@ -2,19 +2,24 @@ from user import User
 
 class Creditials:
     '''
-    Creditials class will perform function to do with authentication with the aid of data from the user class
+    Creditials class will perform 
+    function to do with authentication with
+    the aid of data from the user class
     '''
     def __init__(self):
         '''
-        constructor function used for testing purposes during development
+        constructor function used for testing 
+        purposes during development
         '''
         pass
     
     @classmethod
     def login(cls, username: str, password):
         '''
-        Login function will be used to authenticate the user. 
-        use the saved credials and allow the user to login the appliaction
+        Login function will be used to 
+        find the user. 
+        use the saved credials and allow 
+        the user to login the appliaction
         '''
         for user in User.disply_user_accounts():
             if user.username == username and user.password == password:
@@ -24,3 +29,32 @@ class Creditials:
                 error = f"No such username:({username}) found in the application. Please create an account or check your password!"
                 return error    
         # print(User.disply_user_accounts())
+
+    def password_generate(self, name):
+        '''
+        generate password will generate the user password. 
+        args:
+            name: will receive one parameter of username;
+        method: 
+            will compare. if the username matches the one
+             in the list then the function will display the
+              users password.    
+        '''
+        for user in User.disply_user_accounts():
+            if user.username == name:
+                return user.password
+            else :
+                error = f"No such username:({name}) found in the application. Please create an account\n"
+                return error
+
+    @classmethod
+    def generate_all_saved_accounts(cls):
+        '''
+        generate all saved account function 
+        will be generating the credetials of the user.
+        it will take no arguement
+        '''
+        for account in User.disply_user_accounts():
+            print("***YOUR ACCOUNT DETAILS***")
+            print(f"first name: {account.first_name} \nLast name: {account.last_name} \nUsername: {account.username} \nPassword: {account.password}\n")
+
