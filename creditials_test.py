fa4a4103ebed1541a51fba2c273efaa521cfb4a2
njeri waweru <wawerunjeri714@gmail.com>
2022-01-28 08:58:31 +0300

    added contets in creditials_test.py

--- creditials_test.py
+++ creditials_test.py
@@ -1,2 +1,14 @@
 import unittest 
 from creditials import Creditials
+
+
+class TestCreditials(unittest.TestCase):
+    '''
+    '''
+    def setUp(self):
+        '''
+         Set up method(code) to run before each test cases
+        '''
+        self.new_creditials = Creditials()
+
+   
