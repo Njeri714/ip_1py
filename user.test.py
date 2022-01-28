b26703dbb35155ad74f06bec06a30ecc0154c325
njeri waweru <wawerunjeri714@gmail.com>
2022-01-28 09:38:37 +0300

    add content in user_test.py

--- user.test.py
+++ user.test.py
@@ -16,3 +16,11 @@ class TestUser(unittest.TestCase):
         tear down function for clearing the list after every test
         '''
         User.user = []
+    def test_init(self):
+        '''
+        test_init test the instances of user class before each test runs
+        '''    
+        self.assertEqual(self.new_user.first_name, "Charity")
+        self.assertEqual(self.new_user.last_name, "Waweru)
+        self.assertEqual(self.new_user.username, "njeri")
+        self.assertEqual(self.new_user.password, "714")
